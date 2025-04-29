e1c58eb6275fa27f76393076c2e5228c41d0bac1
Веселин Янков 07 <vesiyankov@gmail.com>
2025-04-29 19:22:15 +0300

    Done Student Business

--- Business/StudentBusiness.cs
+++ Business/StudentBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    internal class StudentBusiness
+    public class StudentBusiness
     {
+        private SchoolRegistryContext context;
+
+        public List<Student> GetAll()
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Students.ToList();
+            }
+        }
+
+        public Student Get(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Students.Find(id);
+            }
+        }
+
+        public void Add(Student student)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Student student)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Students.Find(student.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(student);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Teachers.Find(id);
+                if (item != null)
+                {
+                    context.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
