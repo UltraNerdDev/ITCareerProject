cc015b80629da9f4c8130290d80aac0c3e5a38d7
Веселин Янков 07 <vesiyankov@gmail.com>
2025-04-29 19:26:29 +0300

    Done Grade Business

--- Business/GradeBusiness.cs
+++ Business/GradeBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    internal class GradeBusiness
+    public class GradeBusiness
     {
+        private SchoolRegistryContext context;
+
+        public List<Grade> GetAll()
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Grades.ToList();
+            }
+        }
+
+        public Grade Get(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Grades.Find(id);
+            }
+        }
+
+        public void Add(Grade grade)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                context.Grades.Add(grade);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Grade grade)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Grades.Find(grade.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues( grade);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Grades.Find(id);
+                if (item != null)
+                {
+                    context.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
