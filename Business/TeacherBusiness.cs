d0bd8a0d2fee772833e1a38fc96abe2eaeb17c2e
Веселин Янков 07 <vesiyankov@gmail.com>
2025-04-29 19:10:10 +0300

    Done Teacher Business For Real

--- Business/TeacherBusiness.cs
+++ Business/TeacherBusiness.cs
@@ -8,7 +8,59 @@ using System.Threading.Tasks;
 
 namespace Business
 {
-    internal class TeacherBusiness
+    public class TeacherBusiness
     {
+        private SchoolRegistryContext context;
+
+        public List<Teacher> GetAll()
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Teachers.ToList();
+            }
+        }
+
+        public Teacher Get(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Teachers.Find(id);
+            }
+        }
+
+        public void Add(Teacher teacher)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                context.Teachers.Add(teacher);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Teacher teacher)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Teachers.Find(teacher.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(teacher);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Teachers.Find(id);
+                if (item != null)
+                {
+                    context.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
