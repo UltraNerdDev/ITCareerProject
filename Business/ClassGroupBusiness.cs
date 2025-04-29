477703307ded8ddd242b36b570fc990b7e6358e6
Веселин Янков 07 <vesiyankov@gmail.com>
2025-04-29 19:29:44 +0300

    Done ClassGroup Business

--- Business/ClassGroupBusiness.cs
+++ Business/ClassGroupBusiness.cs
@@ -11,8 +11,59 @@ using Data.Models;
 
 namespace Business
 {
-    internal class ClassGroupBusiness
+    public class ClassGroupBusiness
     {
-        
+        private SchoolRegistryContext context;
+
+        public List<ClassGroup> GetAll()
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Classes.ToList();
+            }
+        }
+
+        public ClassGroup Get(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Classes.Find(id);
+            }
+        }
+
+        public void Add(ClassGroup classGroup)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                context.Classes.Add(classGroup);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(ClassGroup classGroup)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Classes.Find(classGroup.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(classGroup);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Classes.Find(id);
+                if (item != null)
+                {
+                    context.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
