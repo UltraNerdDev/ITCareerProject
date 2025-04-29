3194dab49e4f0b672615a1443951a3d95b4b5ded
Gummie <Gummie@GUMMIE>
2025-04-29 18:46:44 +0300

    Added ParentBusiness class

--- Business/ParentBusiness.cs
+++ Business/ParentBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data.Models;
+using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,57 @@ namespace Business
 {
     internal class ParentBusiness
     {
+        private SchoolRegistryContext context;
+
+        public List<Parent> GetAll()
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Parents.ToList();
+            }
+        }
+
+        public Parent Get(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                return context.Parents.Find(id);
+            }
+        }
+
+        public void Add(Parent parent)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                context.Parents.Add(parent);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Parent parent)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Parents.Find(parent.Id);
+                if (item != null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(parent);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (context = new SchoolRegistryContext())
+            {
+                var item = context.Parents.Find(id);
+                if (item != null)
+                {
+                    context.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
     }
 }
